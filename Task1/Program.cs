e932be63e900ab44290850b0f24e8784118df0bb
laalex <laalex512@gmail.com>
2022-06-19 16:59:32 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+
+int InputInt (string mes)
+{
+    Console.Write($"Insert {mes}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = InputInt("number A");
+int b = InputInt("number B");
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result = result*a;
+}
+Console.WriteLine("A^B = "+result);
