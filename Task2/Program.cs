6b36ef368dd96749d486fb35839857994ec3b329
laalex <laalex512@gmail.com>
2022-06-19 17:00:13 +0300

    add task2 & task3

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход 5 чисел и выдаёт сумму цифр в каждом числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int SumDigit(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
+
+int InputInt (string mes)
+{
+    Console.Write($"Insert {mes}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = InputInt("number1");
+int sum = SumDigit(num);
+Console.WriteLine(sum);
+
+
+
+
+
+
+
---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Вывод сделать отдельным методом.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+void FibonachiArr(int[] a)
+{
+    a[0] = 1;
+    a[1] = 1;
+    for (int i = 2; i < a.Length; i++)
+    {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+}
+
+void PrintArr(int[] a)
+{
+    Console.Write("[");
+    for (int i = 0; i < a.Length - 1; i++)
+    {
+        Console.Write(a[i] + ", ");
+    }
+    Console.Write(a[a.Length-1] + "]");
+    Console.WriteLine();
+}
+
+
+int[] array = new int[8];
+int[] array2 = new int[20];
+
+
+
+FibonachiArr(array);
+FibonachiArr(array2);
+PrintArr(array);
+PrintArr(array2);
+
